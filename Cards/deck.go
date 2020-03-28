4353927cc3cf831e76fb7663c9e352f0b70c869e
gunjanmimo <gunjan.mtbpaul@gmail.com>
2020-03-29 00:37:33 +0530

    new code

--- Cards/deck.go
+++ Cards/deck.go
@@ -10,11 +10,22 @@ type deck []string
 func newDeck() deck {
 	cards := deck{}
 	cardSuit := []string{"Spades", "Hearts", "Diamonds", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Fives", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
+
+	for _, suit := range cardSuit {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+	return cards
 
 }
 
 func (d deck) print() {
-	for i, card := range d {
-		fmt.Println(i, card)
+	for _, card := range d {
+		fmt.Println(card)
 	}
+}
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
 }
