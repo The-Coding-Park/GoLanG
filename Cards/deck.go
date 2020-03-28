a0a598bbfb805ef76d54f6c3fa0a5637920ca165
gunjanmimo <gunjan.mtbpaul@gmail.com>
2020-03-29 00:10:19 +0530

    new

--- Cards/deck.go
+++ Cards/deck.go
@@ -4,9 +4,14 @@ import "fmt"
 
 //create a new type of deck
 //which is a slice of string
-//import "fmt"
 
 type deck []string
+
+func newDeck() deck {
+	cards := deck{}
+	cardSuit := []string{"Spades", "Hearts", "Diamonds", "Clubs"}
+
+}
 
 func (d deck) print() {
 	for i, card := range d {
