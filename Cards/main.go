5dcc2a951b9558358f92d84fb049bd118d070819
gunjanmimo <gunjan.mtbpaul@gmail.com>
2020-03-28 21:19:14 +0530

    new

--- Cards/main.go
+++ Cards/main.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 	// var card string = "Ace of Spades"
 	cards := []string{"Ace of Diamonds", newCard()}
-	cards=append((cards,"six of psrades"))
+	//cards=append((cards,"six of psrades"))
 	fmt.Println(cards)
 }
 
